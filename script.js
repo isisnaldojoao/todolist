b5480151e9cf4582ece7b5dd876f51fd37dad586
isisnaldojoao <isisnaldojoao@gmail.com>
2023-10-24 16:35:09 -0300

    second version

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
-let inTodo = document.getElementById("inTodo")
-let text = document.querySelector(".text")
+// Declarando variáveis
+let inTodo      = document.getElementById("inTodo");
+let text        = document.querySelector(".text");
+let taskOptions = document.getElementById("taskOptions");
 
+// Função para adicionar 
 function add(){
+    // Verificar o valor inválido inserido 
     if(inTodo.value == ""){
         alert("Por favor insira uma Tarefa")
-    }else{
-        let newTask = document.createElement("ul");
-        newTask.innerHTML = `${inTodo.value} <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
-        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
-      </svg>`;
-        text.appendChild(newTask);
-        inTodo.value = "";
-        newTask.querySelector("svg").addEventListener("click" , remove);
-        function remove(event){
-            event.target.parentElement.remove()
-        }
+        return;
     }
+    //Chamar a função para criar uma lista ordenada
+    create(inTodo.value,taskOptions);
+    inTodo.value = "";
+}
+
+//Função para criar elementos (Textos, e seleção)
+function create(){
+    //Criando o elemento com o icone
+    let newTask       = document.createElement("ul");
+    newTask.innerHTML = `${inTodo.value} | ${taskOptions.value} <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-all" viewBox="0 0 16 16">
+    <path d="M8.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L2.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093L8.95 4.992a.252.252 0 0 1 .02-.022zm-.92 5.14.92.92a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 1 0-1.091-1.028L9.477 9.417l-.485-.486-.943 1.179z"/>
+    </svg>`;
+    //Adicionando o elemento nó a outro nó
+    text.appendChild(newTask);
+    newTask.style.wordSpacing = "20px";
+    inTodo.value = "";
+    /*Chamar a função para deleter um elemento da lista*/
+    newTask.querySelector("svg").addEventListener("click" , function(){
+        remove(newTask)
+    });
+}
+
+//Função para remover um nó dentro da list
+function remove(newTask){
+    newTask.remove();
 }
